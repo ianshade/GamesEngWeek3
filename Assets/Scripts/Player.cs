4b082807392f7ce399562dac71085b3e0d055c99
ianshade <zegzulakrzysztof@gmail.com>
2020-04-28 00:15:27 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    private Animator myAnimator;
+    private Rigidbody2D myRigidbody;
+    private Vector2 movementFromInput;
+
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        movementFromInput.x = Input.GetAxis("Horizontal");
+        movementFromInput.y = Input.GetAxis("Vertical");
+        myAnimator.SetFloat("X", movementFromInput.x);
+        myAnimator.SetFloat("Y", movementFromInput.y);
+        myAnimator.SetFloat("Velocity", movementFromInput.magnitude);
+        myRigidbody.velocity = movementFromInput * movementSpeed;
+
+    }
+}
